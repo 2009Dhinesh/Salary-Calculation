e82613e061401f7d71e58857ea29ed6e580d167c
Dhinesh Waran P <dhineshwaran1712@gmail.com>
2026-03-24 16:11:04 +0530

    Delete src directory

--- src/routes/wealthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/auth');
-const { getWealthDashboard } = require('../controllers/wealthController');
-
-router.use(protect);
-router.get('/dashboard', getWealthDashboard);
-
-module.exports = router;
